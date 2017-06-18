b1cbff3728523ffbcb907f7df66133bc8efd821a
Ben Hadfield <Ben@hadfieldfamily.co.uk>
2017-06-18 13:03:14 +0100

    primitives file uses docstring

--- src/services/primitives/index.js
+++ src/services/primitives/index.js
@@ -1,5 +1,10 @@
-// converts an array of objects to an object, where the keys are specified
-// by `id` and the values are specified by `getValue` function.
+/**
+ * Converts an array of objects to an object, where the keys are specified by `id` and
+ * the values are specified by `getValue` function.
+ * @param {array} array - array of objects to convert
+ * @param {string} key - key in objects to key the returned object
+ * @param {function} valueFunc - passed each object and should return the value to use for that object
+ */
 export const objectFromArray = (array, key, valueFunc = () => {}) => {
   return array.reduce((acc, item) => {
     if (item[key] !== undefined) {
@@ -9,8 +14,10 @@ export const objectFromArray = (array, key, valueFunc = () => {}) => {
   }, {})
 }
 
-// takes an object, key and value function and returns a new object with
-// the value specified by the given key replaced with the given value.
+/**
+ * Takes an object, key and value function and returns a new object with the
+ * value specified by the given key replaced with the given value.
+ */
 export const objFrom = (obj, key, valueFunc) => {
   return {
     ...obj,
@@ -18,4 +25,5 @@ export const objFrom = (obj, key, valueFunc) => {
   }
 }
 
+/** Generates a random string. */
 export const randomString = () => Math.random().toString(36).replace(/[^a-z]+/gi, '')

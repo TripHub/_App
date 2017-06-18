4c6a3b1112cf1f0d6d201f7bb5c2dd87bbc0b66a
Ben Hadfield <Ben@hadfieldfamily.co.uk>
2017-06-18 13:47:39 +0100

    more docstrings 🤗

--- src/services/api/index.js
+++ src/services/api/index.js
@@ -1,7 +1,12 @@
 import { CALL_API } from 'redux-api-middleware'
 
-// sets defaults then updates with given config
-export const apiRequest = (endpoint, config) => ({
+/**
+ * Sets useful request defaults, and then updates with given config.
+ * @param {string} endpoint - The pathname of the endpoint to call
+ * The REACT_APP_API_URL from .env is prepended.
+ * @param {object} [config={}] - Additional request configuration.
+ */
+export const apiRequest = (endpoint, config = {}) => ({
   [CALL_API]: {
     endpoint: process.env.REACT_APP_API_URL + endpoint,
     method: 'get',

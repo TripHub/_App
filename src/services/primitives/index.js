a69887d028611075e7baeda1c6b24eac0a7914f0
Ben Hadfield <Ben@hadfieldfamily.co.uk>
2017-06-18 13:11:00 +0100

    simplified comments in primitives

--- src/services/primitives/index.js
+++ src/services/primitives/index.js
@@ -1,9 +1,9 @@
 /**
- * Converts an array of objects to an object, where the keys are specified by `id` and
- * the values are specified by `getValue` function.
+ * Converts an array of objects to an object.
  * @param {array} array - array of objects to convert
  * @param {string} key - key in objects to key the returned object
- * @param {function} valueFunc - passed each object and should return the value to use for that object
+ * @param {function} valueFunc - passed each object and should return the
+ * value to use for that object
  */
 export const objectFromArray = (array, key, valueFunc = () => {}) => {
   return array.reduce((acc, item) => {
@@ -15,10 +15,10 @@ export const objectFromArray = (array, key, valueFunc = () => {}) => {
 }
 
 /**
- * Takes an object, key and value function and returns a new object with the
- * value specified by the given key replaced with the given value.
+ * Takes an object and returns a new object with the value specified by the
+ * given key replaced with the given value.
  */
-export const objFrom = (obj, key, valueFunc) => {
+export const updateObject = (obj, key, valueFunc) => {
   return {
     ...obj,
     ...key && { [key]: valueFunc(obj[key]) }
@@ -26,4 +26,6 @@ export const objFrom = (obj, key, valueFunc) => {
 }
 
 /** Generates a random string. */
-export const randomString = () => Math.random().toString(36).replace(/[^a-z]+/gi, '')
+export const randomString = () => (
+  Math.random().toString(36).replace(/[^a-z]+/gi, '')
+)

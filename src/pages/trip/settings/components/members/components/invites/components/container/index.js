536bd3ab03252cda59eb9f54cf3198f0bad34f93
Ben Hadfield <Ben@hadfieldfamily.co.uk>
2017-06-23 22:41:34 +0100

    conform to standard.js

--- src/pages/trip/settings/components/members/components/invites/components/container/index.js
+++ src/pages/trip/settings/components/members/components/invites/components/container/index.js
@@ -1,5 +1,5 @@
 import { styled } from 'styletron-react'
-import { opacity } from '../../../../../../../../../common/style';
+import { opacity } from '../../../../../../../../../common/style'
 
 export default styled('div', ({ loading }) => ({
   opacity: loading ? opacity.disabled : 1

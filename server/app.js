30894e21bdd81022955df26a8c74e6007910a7e4
Ben Hadfield <Ben@hadfieldfamily.co.uk>
2017-07-04 10:02:08 +0100

    added docs to /server

--- server/app.js
+++ server/app.js
@@ -1,12 +1,9 @@
 const express = require('express')
 const path = require('path')
-
 const app = express()
 
 app.use(express.static(path.join(__dirname, '..', 'build')))
 
-// we need all routes to give index.html so client-side routing works
-// as described at http://bit.ly/2rbPw2W
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'build', 'index.html'))
 })
--- server/index.js
+++ server/index.js
@@ -1,3 +1,10 @@
+/**
+ * The frontend server is really really (really) simple.
+ * All it does is map every pathname to /index.html so that client-side routing
+ * can take over. Without this, certain callback routes would give a 404
+ * in production, as described in the CRA docs: http://bit.ly/2rbPw2W.
+ */
+
 const app = require('./app')
 
 const PORT = process.env.PORT || 3001

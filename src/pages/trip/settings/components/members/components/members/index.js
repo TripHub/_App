268b57b416b99b597b8e65a63fcaa6c3427a4de5
Ben Hadfield <Ben@hadfieldfamily.co.uk>
2017-07-06 11:40:36 +0100

    more prop-type checking 🙃

--- src/pages/trip/settings/components/members/components/members/index.js
+++ src/pages/trip/settings/components/members/components/members/index.js
@@ -1,22 +1,31 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import { Text } from '../../../../../../../components/text'
 import Table from './components/table'
 
-export default ({ members, ...props }) => (
+const Members = ({ members, ...props }) => (
   <Table>
     <tbody>
-      {
-        members.map((member) => (
-          <tr>
-            <td>
-              <Text>{member.email}</Text>
-            </td>
-            <td>
-              <Text>member</Text>
-            </td>
-          </tr>
-        ))
-      }
+      {members.map((member) => (
+        <tr key={member.id}>
+          <td>
+            <Text>{member.email}</Text>
+          </td>
+          <td>
+            <Text>member</Text>
+          </td>
+        </tr>
+      ))}
     </tbody>
   </Table>
 )
+
+Members.propTypes = {
+  members: PropTypes.arrayOf(PropTypes.object)
+}
+
+Members.defaultProps = {
+  members: []
+}
+
+export default Members

c1a9213d24fa485d5880f67f27039d5147d74411
Ben Hadfield <benjohnhadfield@gmail.com>
2017-08-05 21:20:10 +0100

    added trips loading (#12)

--- src/components/spinner/index.js
+++ src/components/spinner/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { color } from '../../common/style'
+import { P } from '../text'
 import Container from './components/container'
 import Spinner from './components/spinner'
 
 export default (props) => (
   <Container>
+    <P>Loading…</P>
     <Spinner name='three-bounce' color={color.black} {...props} />
   </Container>
 )
